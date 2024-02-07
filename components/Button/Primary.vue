c179be169767183d6c121f5211356ad39ba96661
Niil <91337648+neeeal@users.noreply.github.com>
2024-02-07 16:31:56 +0800

    feat: primary button and initialized seconday button

---
+++ components/Button/Primary.vue
@@ -0,0 +1,17 @@
+<template>
+    {{ console.log(bgColor) }}
+    <button v-if="active ==='on'" class="bg-blue-900 text-white text-sm  border w-11/12 h-10 rounded-2xl mx-auto my-3.5">button</button>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    active: {
+        type: String
+    }
+    // bgColor: {
+    //     type: String,
+    //     default: "bg-blue-900"
+    // }
+})
+</script>
