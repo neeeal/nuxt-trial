b7cedc7eaef9950495e3959b1cd6e21eac2cc12f
Niil <91337648+neeeal@users.noreply.github.com>
2024-02-07 16:33:26 +0800

    feat: added default layout for header and navigation

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col min-h-[15vh] border border-10 border-black">
+        <div class="flex flex-1 ">
+            QCU Logo
+        </div>
+        <div class="flex flex-row flex-1 pt-4 border">
+            
+        </div>
+    </div>
+    <slot />
+  </template>
+  
