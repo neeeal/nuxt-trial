a6a9951120b6cb8f128a62f236b5d8e84b60da5f
Niil <91337648+neeeal@users.noreply.github.com>
2024-02-10 00:08:43 +0800

    refactor: updated banner responsive

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex relative bg-slate-800 overflow-hidden min-h-[45vh]">
-            <div class="flex flex-col w-1/2 justify-center flex-grow pl-12  z-50 ">
+    <div class="flex relative bg-slate-800 overflow-hidden min-h-[20rem]">
+            <div class="flex flex-col w-1/2 justify-center flex-grow pl-24   z-50 ">
                 <h4 class="text-xs text-white">Quezon City University’s Directory of Journals</h4>
                 <h1 class="py-2 text-2xl text-white">Find or Submit Open Access Articles</h1>
                 <div class="flex flex-row">
