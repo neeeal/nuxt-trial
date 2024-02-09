2d626919026506d4522ed028b5aee25849a9b714
Niil <91337648+neeeal@users.noreply.github.com>
2024-02-10 03:56:47 +0800

    feat: popular articles section

---
+++ components/Card/Article.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div class="max-w-[20rem] max-h-[20rem] overflow-hidden mx-[0.2rem] border-x-4 border-y-[1px] rounded-md px-[1.25rem] py-[2rem] text-xs">
+        <h3 class=" text-blue-900 font-semibold text-sm">Blockchain Beyond Cryptocurrency: Transforming Industries with Distributed Ledger Technology</h3>
+        <div class="flex flex-row justify-between text-slate-600 opacity-50">
+            <label class="pb-2 pt-1">THE LAMP</label>
+            <label>103 views</label>
+        </div>
+        <h4 class=" text-slate-600">By Jane Dela Cruz</h4>
+        <p class="text-justify py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiuxsod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ali..</p>
+        <button class="border-[1.5px] border-[#285581] min-w-full rounded-2xl  py-[0.4rem] text-blue-900">
+            Read Article
+        </button>
+
+    </div>
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,40 @@
         <!-- Hero Section -->
         <Banner />
 
-        <Section> Section </Section>
+        <Section>
+            <div class="p-[4rem] pt-[3rem]">
+
+                <h2 class="font-semibold text-blue-800 text-lg">Popular Articles this Month</h2>
+
+                <div class="flex pt-[2rem]">
+
+                    <CardArticle/>
+                    <CardArticle/>
+                    <div class="border-2 rounded p-[1rem] pb-0 ml-[4rem] text-sm">
+                        <h3 class="font-bold pb-[1.10rem]">Most Downloaded Articles</h3>
+                        <ul class="flex flex-col justify-between text-blue-900 font-semibold  ">
+                            <li v-for="popular in populars" class="flex pb-[1rem]">
+                                <div class="flex-grow-2">
+                                    <label>{{popular.title}}</label>
+                                </div>
+                                <div class="flex-grow flex-col px-[0.5rem] pt-[1rem]">
+                                    <label class="flex justify-center -mb-[0.5rem] ">{{popular.views}}</label>
+                                    <label class="flex text-[8px]  justify-center text-slate-600 opacity-50">VIEWS</label>
+                                </div>
+                                <div class="flex-grow flex-col px-[2.25rem] pt-[1rem]">
+                                    <label class="flex justify-center -mb-[0.5rem]">{{popular.downloads}}</label>
+                                    <label class="flex text-[8px] justify-center text-slate-600 opacity-50">DOWNLOADS</label>
+                                </div>
+                            </li>
+                        </ul>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+        </Section>
 
         <Section> Section </Section>
         
@@ -38,4 +71,22 @@
 
 <script setup>
 const active = "on"
+
+const populars = [
+    {
+        title:"Blockchain Beyond Cryptocurrency: Transforming Industries with Distributed Ledger Technology...",
+        views:"10.2k",
+        downloads:"48k"
+    },
+    {
+        title:"Blockchain Beyond Cryptocurrency: Transforming Industries with Distributed Ledger Technology...",
+        views:"10.2k",
+        downloads:"48k"
+    },
+    {
+        title:"Blockchain Beyond Cryptocurrency: Transforming Industries with Distributed Ledger Technology...",
+        views:"10.2k",
+        downloads:"48k"
+    },
+]
 </script>
